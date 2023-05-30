526905a3d4e21fbaf0902501dc36b73ce6e8c94d
Alper alanyalie <30805005+alperalanyali@users.noreply.github.com>
2023-05-30 10:06:18 +0300

    2

---
+++ WebApi/Controllers/CarController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Features.CarFeatures.Commands.CreateCar;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Abstractions;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    public class CarController : ApiController
+    {
+        public CarController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpPost("CreateCar")]
+        public async Task<ActionResult> CreateCar(CreateCarCommand request,CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+            return Ok(response);
+        }        
+    }
+}
+
