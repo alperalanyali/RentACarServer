8647a98175d355705f9c15d4d14aa6176b60ff5e
Alper alanyalie <30805005+alperalanyali@users.noreply.github.com>
2023-05-31 10:50:17 +0300

    Jwt token added

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -7,6 +7,8 @@ namespace Domain.Entities
 	public class User:IdentityUser<Guid>
 	{
 		public string FullName { get; set; }
+		public string RefreshToken { get; set; }
+		public DateTime RefreshTokenExpires { get; set; }
 		public Status Statu { get; set; }
 	}
 }
---
+++ Infrastructure/Authentication/JwtOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Infrastructure.Authentication
+{
+	public class JwtOptions
+	{
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string SecretKey { get; set; }
+    }
+}
+
